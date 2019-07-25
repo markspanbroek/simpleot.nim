27e6fd8a14282a5c8a2cd647816e9e25e32354f4
Mark Spanbroek <mark@spanbroek.net>
2019-07-25 08:24:48 +0200

    Cleanup

--- simpleot.nim
+++ simpleot.nim
@@ -29,23 +29,23 @@ cImport(src/"ot_config.h")
 cImport(src/"randombytes.h")
 cImport(src/"ot_sender.h")
 
+# Nimterop doesn't support multidimensional arrays yet
+# https://github.com/nimterop/nimterop/issues/54
 cOverride:
-  # Nimterop doesn't support multidimensional arrays yet
-  # https://github.com/nimterop/nimterop/issues/54
-  const name = "SIMPLEOT_RECEIVER"
-  const header = src/"ot_receiver.h"
   type
-    SIMPLEOT_RECEIVER* {.importc: name, header: header, bycopy.} = object
+    SIMPLEOT_RECEIVER* {.
+      importc: "SIMPLEOT_RECEIVER", header: src/"ot_receiver.h", bycopy
+    .} = object
       S_pack* {.importc: "S_pack".}: array[PACKBYTES, cuchar]
       S* {.importc: "S".}: ge4x
       table* {.importc: "table".}: array[64 div DIST, array[8, ge4x]]
       xB* {.importc: "xB".}: ge4x
       x* {.importc: "x".}: array[4, sc25519]
-  proc sender_keygen*(a1: ptr SIMPLEOT_SENDER; a2: ptr cuchar;
-      a3: ptr array[4, array[HASHBYTES, cuchar]]) {.stdcall,
-      importc: "sender_keygen", header: src/"ot_sender.h".}
-  proc receiver_keygen*(a1: ptr SIMPLEOT_RECEIVER; a2: ptr array[HASHBYTES, cuchar]) {.
-    stdcall, importc: "receiver_keygen", header: src/"ot_receiver.h".}
-
+  proc sender_keygen*(s: ptr SIMPLEOT_SENDER, Rs_pack: ptr cuchar,
+    keys: ptr array[4, array[HASHBYTES, cuchar]])
+    {.stdcall, importc: "sender_keygen", header: src/"ot_sender.h".}
+  proc receiver_keygen*(r: ptr SIMPLEOT_RECEIVER, 
+    keys: ptr array[HASHBYTES, cuchar])
+    {.stdcall, importc: "receiver_keygen", header: src/"ot_receiver.h".}
 
 cImport(src/"ot_receiver.h")
